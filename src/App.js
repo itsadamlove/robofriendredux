26aef2067a97c418652368017214b18f39e86991
Adam Love <adam.love127@gmail.com>
2018-08-28 20:37:50 +1000

    Fetch users from jsonplaceholder

--- src/App.js
+++ src/App.js
@@ -8,9 +8,15 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({robots: users}))
   }
 
   onSearchChange = (e) => {
@@ -21,14 +27,17 @@ class App extends Component {
     const filteredRobots = this.state.robots.filter(robot => {
       return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
     })
-
-    return(
-      <div className="tc">
-        <h1 className="f2">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
-        <Cardlist robots={filteredRobots}/>
-      </div>
-    )
+    if (this.state.robots.length === 0) {
+      return <h1>Loading...</h1>
+    } else {
+      return(
+        <div className="tc">
+          <h1 className="f2">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange}/>
+          <Cardlist robots={filteredRobots}/>
+        </div>
+      )
+    }
   }
 }
 
