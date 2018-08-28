6b96a3ed8b8ac4749bf74b737450ff7b6565de90
Adam Love <adam.love127@gmail.com>
2018-08-28 20:23:00 +1000

    Add styling and sega font

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component }from 'react'
 import Cardlist from './Cardlist'
 import {robots} from './robots.js'
 import SearchBox from './SearchBox.js'
+import './App.css'
 
 class App extends Component {
   constructor() {
@@ -23,7 +24,7 @@ class App extends Component {
 
     return(
       <div className="tc">
-        <h1>RoboFriends</h1>
+        <h1 className="f2">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange}/>
         <Cardlist robots={filteredRobots}/>
       </div>
